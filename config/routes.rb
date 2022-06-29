0b3f2c0c6a229d84caba2f4b76dc1ba59f1bc6e9
gautamgoshopmatic <gautam.fonseca@goshopmatic.com>
2022-06-29 15:46:14 +0530

    added api controller - apis for airline and review works fine now , tested in postman

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   root 'pages#index'
 
   namespace :api do
-    namepace :v1 do
+    namespace :v1 do
       resources :airlines, param: :slug
-      resources :review, only [:create, :destroy]
+      resources :reviews, only: [:create, :destroy]
     end
   end
+
 
 
   get '*path', to: 'pages#index', via: :all
